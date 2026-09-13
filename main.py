8dcc95270fec264cd62b6d9330da4ec5548dba46
thehcj <260060686+thehcj@users.noreply.github.com>
2026-09-13 10:38:05 +0100

    fix conditions and add comments

--- main.py
+++ main.py
@@ -12,16 +12,17 @@ lose = 0
 while True:
     plr = input("Enter rock, paper, or scissors: ")
     bot = random.randint(1,9)
+    # I have intentionally skewed the probability of a win by making the computer pick a number between 1-9
 
-    if plr == "rock" or plr == "scissors" or plr == "scissors":
-        if bot == 4 or bot == 6 or bot == 8:
+    if plr in cases.keys:
+        if bot == 4 or bot == 6 or bot == 8: # These numbers are 3 random numbers I thought of in range to give a probability of a win
             print(f"{plr} beats {cases[plr]}!")
             win += 1
-        elif bot == 5:
+        elif bot == 5: # This is a random number to give a probability of a draw 
             print(f"{plr} draws {plr}")
             draw += 1
-        else:
-            print(f"{plr} loses to {cases[cases[plr]]}")
+        else: # Anything that is outside the numbers above are losses
+            print(f"{plr} loses to {cases[cases[plr]]}") # I indirectly find what the player will lose to
             lose += 1
     
 
